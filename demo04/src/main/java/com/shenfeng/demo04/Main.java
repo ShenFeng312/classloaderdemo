604d438e83a7efa89550180cb0c223e9df514279
shenfeng <shenfeng197@gmail.com>
2025-07-25 11:07:45 +0800

    添加注释

--- demo04/src/main/java/com/shenfeng/demo04/Main.java
+++ demo04/src/main/java/com/shenfeng/demo04/Main.java
@@ -1,5 +1,15 @@
 package com.shenfeng.demo04;
 
+/**
+ *  java -jar ./demo04/target/demo04-1.0-SNAPSHOT.jar
+ */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(Main.class.getClassLoader().getClass().getName());
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            System.out.println(stackTraceElement);
+        }
+    }
 
 }
